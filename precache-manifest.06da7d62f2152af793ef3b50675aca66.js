04f01ccc7bb80709192ad62649c37d140a7790d4
yeongbba <yeongmolee2@gmail.com>
2020-12-03 20:21:44 +0900

    Updates

---
+++ precache-manifest.06da7d62f2152af793ef3b50675aca66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf43c166e2dd0537ec1a304e7299e995",
+    "url": "/React-class__youtube/index.html"
+  },
+  {
+    "revision": "422b798cca83c37be514",
+    "url": "/React-class__youtube/static/css/main.12e58ce2.chunk.css"
+  },
+  {
+    "revision": "111a3aaf298562895621",
+    "url": "/React-class__youtube/static/js/2.92b5070a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-class__youtube/static/js/2.92b5070a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422b798cca83c37be514",
+    "url": "/React-class__youtube/static/js/main.04558932.chunk.js"
+  },
+  {
+    "revision": "9965398d6ea662cb3efe",
+    "url": "/React-class__youtube/static/js/runtime-main.1938206a.js"
+  }
+]);
